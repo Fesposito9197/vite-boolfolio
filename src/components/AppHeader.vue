5546c7fd0f83493c990df8f8f6317059a116b675
Fesposito9197 <kekkoesp8@gmail.com>
2023-02-02 15:26:06 +0100

    aggiunge nav bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,6 +5,13 @@ export default {
 </script>
 <template>
   <h1>Header</h1>
+  <nav>
+    <ul>
+      <li><RouterLink to="/">HomePage</RouterLink></li>
+      <li><RouterLink to="about-us">About Us</RouterLink></li>
+      <li><RouterLink to="contact-us">Contact Us</RouterLink></li>
+    </ul>
+  </nav>
 </template>
 
 <style lang="scss" scoped></style>
