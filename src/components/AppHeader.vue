4f242c76c96a531c30ceda3e602ccee6a3ae9b8b
Fesposito9197 <kekkoesp8@gmail.com>
2023-02-04 00:25:52 +0100

    aggiunge stile nav bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,14 +4,44 @@ export default {
 };
 </script>
 <template>
-  <h1>Header</h1>
-  <nav>
-    <ul>
-      <li><RouterLink :to="{ name: 'homepage' }">HomePage</RouterLink></li>
-      <li><RouterLink :to="{ name: 'about-us' }">About Us</RouterLink></li>
-      <li><RouterLink :to="{ name: 'contact-us' }">Contact Us</RouterLink></li>
-    </ul>
-  </nav>
+  <header class="bg-primary bg-gradient">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <RouterLink :to="{ name: 'homepage' }" class="nav-link active">
+                  HomePage
+                </RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink :to="{ name: 'about-us' }" class="nav-link">
+                  About Us
+                </RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink :to="{ name: 'contact-us' }" class="nav-link">
+                  Contact Us
+                </RouterLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped></style>
